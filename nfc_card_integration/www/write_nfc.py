55eab5114b88974efc3226d644b773991da224ad
Ahmed Emam <aemam@creativeadvtech.com>
2025-05-14 16:35:54 +0000

    Updates

--- nfc_card_integration/www/write_nfc.py
+++ nfc_card_integration/www/write_nfc.py
@@ -1,10 +1,10 @@
 import frappe
 
 def get_context(context):
-    # Get the card_id from the URL
-    card_id = frappe.form_dict.get('card_id') or frappe.request.path.split('/')[-1]
-    # Fetch NFC Card record
-    card = frappe.get_doc("NFC Card", {"card_id": card_id})
-    context.card = card
-    context.no_cache = 1
+    card_id = frappe.request.path.rstrip('/').split('/')[-1]
+    try:
+        card = frappe.get_doc("NFC Card", {"card_id": card_id, "disabled": 0})
+        context.card = card
+    except frappe.DoesNotExistError:
+        context.card = None
     return context
